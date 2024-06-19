1069121e1cfac2123c9f67dd45b506700460a3de
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-06-19 15:34:18 +0200

    chore: add script for listing all modules

---
+++ scripts/list_modules.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# List all Equinor Terraform Baseline modules as GitHub flavored Markdown checkboxes.
+# Useful when creating Issues that require updating all module repositories.
+#
+# Prerequisites:
+#   gh
+
+set -eu
+
+readarray -t repos <<< "$(gh repo list equinor --topic terraform-module,terraform-baseline --json nameWithOwner,homepageUrl | jq -c .[])"
+
+for repo in "${repos[@]}"; do
+  repo_name=$(echo "$repo" | jq -r .nameWithOwner)
+  namespace=$(echo "$repo_name" | cut -d / -f 1)
+  provider=$(echo "$repo_name" | cut -d - -f 2)
+  module_name=$(echo "$repo_name" | cut -d - -f 3-)
+  module_url=$(echo "$repo" | jq -r .homepageUrl)
+  echo "- [ ] [$namespace/$module_name/$provider]($module_url)"
+done
