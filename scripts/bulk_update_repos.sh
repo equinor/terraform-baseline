062d278919ee0763a91d51ae0a1bb3a5ca29e97d
Helena Solberg Kallekleiv <110530547+helenakallekleiv@users.noreply.github.com>
2024-07-04 16:03:26 +0200

    chore: update bulk script (#170)

--- scripts/bulk_update_repos.sh
+++ scripts/bulk_update_repos.sh
@@ -14,7 +14,7 @@ branch_name=$2
 commit_message=$3
 
 owner="equinor"
-repos=$(gh repo list "$owner" --visibility public --topic terraform-module,terraform-baseline --limit 999999 --json name --jq .[].name)
+readarray -t repos <<<"$(gh repo list "$owner" --visibility public --topic terraform-module,terraform-baseline --limit 999999 --json name --jq .[].name)"
 root=$(pwd)
 
 for repo in "${repos[@]}"; do
@@ -22,19 +22,21 @@ for repo in "${repos[@]}"; do
   git clone "https://github.com/$owner/$repo.git"
   cd "$repo" || exit
 
-  # Create and switch to new branch
+  # Create and switch to new branch.
+  ## When rerunning the script to update an existing branch, remove '--create'.
   git switch --create "$branch_name"
 
   # Run script to make changes
-  # shellcheck source=/dev/null
+  ## shellcheck source=/dev/null
   source "$root/$script_path"
 
   # Commit all changes
   git add --all
   git commit --message "$commit_message"
-  git push
+  git push -u origin "$branch_name"
 
   # Create PR
+  ## When rerunning the script to update an existing branch, comment out the line below.
   gh pr create --title "$commit_message" --body ""
 
   # Delete local repo
