bf05d6ea86a4f6167e3c60fd660e1dc60200af54
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2025-09-30 16:21:15 +0200

    Revert "clarify badge colors"
    This reverts commit 4656e3a4736a457f4e54f13d676f56690c843da8.

--- scripts/create_badges.sh
+++ scripts/create_badges.sh
@@ -1,26 +1,30 @@
 #! /bin/bash
 #
 # In a logical order:
-# 1. Blue: latest release
-# 2. Bright green: community stats (downloads and contributors)
-# 3. Yellow: maintenance stats (issues and pull requests)
-# 4. Green: license
+# 1. Module status
+#     - GitHub release tag
+#     - Terraform module downloads
+# 2. Community and maintenance
+#     - Contributors
+#     - Issues
+#     - Pull requests
+# 3. License
 
 set -eu
 
-module_name="$1"
-provider="${2:-"azurerm"}"
+module_name=$1
+provider=${2:-"azurerm"}
 
 namespace="equinor"
 repo="$namespace/terraform-${provider}-${module_name}"
 module="$namespace/$module_name/$provider"
 badges="
-[![GitHub Release](https://img.shields.io/github/v/release/$repo?color=blue)](https://github.com/$repo/releases/latest)
-[![Terraform Module Downloads](https://img.shields.io/terraform/module/dt/$module?color=brightgreen)](https://registry.terraform.io/modules/$module/latest)
-[![GitHub contributors](https://img.shields.io/github/contributors/$repo?color=brightgreen)](https://github.com/$repo/graphs/contributors)
-[![GitHub Issues](https://img.shields.io/github/issues/$repo?color=yellow)](https://github.com/$repo/issues)
-[![GitHub Pull requests](https://img.shields.io/github/issues-pr/$repo?color=yellow)](https://github.com/$repo/pulls)
-[![GitHub License](https://img.shields.io/github/license/$repo?color=green)](https://github.com/$repo/blob/main/LICENSE)
+[![GitHub Release](https://img.shields.io/github/v/release/$repo)](https://github.com/$repo/releases/latest)
+[![Terraform Module Downloads](https://img.shields.io/terraform/module/dt/$module)](https://registry.terraform.io/modules/$module/latest)
+[![GitHub contributors](https://img.shields.io/github/contributors/$repo)](https://github.com/$repo/graphs/contributors)
+[![GitHub Issues](https://img.shields.io/github/issues/$repo)](https://github.com/$repo/issues)
+[![GitHub Pull requests](https://img.shields.io/github/issues-pr/$repo)](https://github.com/$repo/pulls)
+[![GitHub License](https://img.shields.io/github/license/$repo)](https://github.com/$repo/blob/main/LICENSE)
 "
 
 echo "$badges"
