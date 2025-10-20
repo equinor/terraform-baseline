e897d8857b7899d8485407fcff2844fcf626cb40
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2025-10-20 07:45:59 +0200

    reorder badges

--- scripts/create_badges.sh
+++ scripts/create_badges.sh
@@ -4,9 +4,9 @@
 #
 # Creates badges in a logical order:
 #   1. Latest release
-#   2. Downloads
-#   3. Community and maintenance (contributors, issues and pull requests)
-#   4. License
+#   2. License
+#   3. Downloads
+#   4. Community and maintenance (contributors, issues and pull requests)
 
 set -eu
 
@@ -18,11 +18,11 @@ repo="$namespace/terraform-${provider}-${module_name}"
 module="$namespace/$module_name/$provider"
 badges="
 [![GitHub Release](https://img.shields.io/github/v/release/$repo)](https://github.com/$repo/releases/latest)
+[![GitHub License](https://img.shields.io/github/license/$repo?color=blue)](https://github.com/$repo/blob/main/LICENSE)
 [![Terraform Module Downloads](https://img.shields.io/terraform/module/dt/$module)](https://registry.terraform.io/modules/$module/latest)
 [![GitHub contributors](https://img.shields.io/github/contributors/$repo)](https://github.com/$repo/graphs/contributors)
 [![GitHub Issues](https://img.shields.io/github/issues/$repo)](https://github.com/$repo/issues)
 [![GitHub Pull requests](https://img.shields.io/github/issues-pr/$repo)](https://github.com/$repo/pulls)
-[![GitHub License](https://img.shields.io/github/license/$repo)](https://github.com/$repo/blob/main/LICENSE)
 "
 
 echo "$badges"
